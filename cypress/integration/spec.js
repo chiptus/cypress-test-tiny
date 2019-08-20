1ddfc7fb9bb7baf2928fdc9d6a19250861a02513
Chaim Lev-Ari <chaim@cnvrg.io>
2019-08-20 09:50:47 +0300

    add failing test

--- cypress/integration/spec.js
+++ cypress/integration/spec.js
@@ -1,3 +1,11 @@
-describe('page', () => {
-  it('works', () => {})
-})
+/// <reference types="Cypress" />
+
+describe('testn parsing of html', () => {
+  it('should succeed', () => {
+    const $html = Cypress.$(`<html><body>
+        <div class="hello">hey</div>
+        
+        </body></html>`);
+    expect($html.find('div.hello').length).to.equal(1);
+  });
+});
